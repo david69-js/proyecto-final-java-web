3b173cc27e6c29901d1567a7267ce56b6985f3d0
DAVID_TOJ\da390 <da3909622@gmail.com>
2023-10-17 11:37:11 -0600

    Main page file

--- src/main/java/com/example/paginaWebUniversidad/PaginaWebUniversidadApplication.java
+++ src/main/java/com/example/paginaWebUniversidad/PaginaWebUniversidadApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PaginaWebUniversidadApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PaginaWebUniversidadApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PaginaWebUniversidadApplication.class, args);
+    }
 }
