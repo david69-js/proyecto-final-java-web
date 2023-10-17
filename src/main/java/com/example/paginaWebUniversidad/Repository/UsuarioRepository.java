acf5e5355747c6b7b10d04507a50d9859784800d
DAVID_TOJ\da390 <da3909622@gmail.com>
2023-10-17 12:54:13 -0600

    usuario repository

--- src/main/java/com/example/paginaWebUniversidad/Repository/UsuarioRepository.java
+++ src/main/java/com/example/paginaWebUniversidad/Repository/UsuarioRepository.java
@@ -2,4 +2,6 @@ package com.example.paginaWebUniversidad.Repository;
 import com.example.paginaWebUniversidad.Model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+    public Usuario findByEmail(String email);
 }
